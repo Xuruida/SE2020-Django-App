682f7101f12f7ae03840f060e01ab682f990c292
XuRuida <2475503376@qq.com>
2020-05-10 01:15:59 +0800

    Modified views to use django.contrib.auth.models.User. But still have no JSON output.

--- interface_app/user_auth/views.py
+++ interface_app/user_auth/views.py
@@ -4,6 +4,11 @@ from .models import UserInfo
 
 # avoid error 403
 from django.views.decorators.csrf import csrf_exempt
+
+# import auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
 # Create your views here.
 
 @csrf_exempt
@@ -12,12 +17,11 @@ def signup(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         try:
-            UserInfo.objects.get(username=username)
+            User.objects.get(username=username)
             return HttpResponse("Username: %s has already existed." % username)
-        except BaseException:
-            newUser = UserInfo(username=username, password=password)
-            newUser.save()
-            return HttpResponse("Created new user %s!\n" % username)
+        except:
+            User.objects.create_user(username, password=password)
+            return HttpResponse("Created new user: %s!\n" % username)
     return HttpResponse("Please use POST method to signup.")
 
 @csrf_exempt
@@ -25,15 +29,13 @@ def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         if not username:
-            return HttpResponse("Please input username")
+            return HttpResponse("Please post username")
         password = request.POST.get('password')
         if not password:
-            return HttpResponse("Please input password")
-        try:
-            user_info = UserInfo.objects.get(username=username)
-            if user_info.password == password:
-                return HttpResponse("Password correct. Login succeeded.")
-            else:
-                return HttpResponse("Password incorrect. Login failed.")
-        except BaseException:
-            return HttpResponse("User: %s has not register. Please sign up." % username)
+            return HttpResponse("Please post password")
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return HttpResponse("Password correct. Login succeeded.")
+        else:
+            return HttpResponse("username or password is not correct")
